81dddf90a4b78101781247cd5c2a585c9e49e413
Naoya Miyagawa <miyagawa@fusic.co.jp>
2020-12-17 02:35:34 +0900

    build css with sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,41 @@
-const path = require('path');
-const mode = 'development';
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const mode = "development";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: mode,
-  entry: './src/javascripts/main.js',
+  entry: "./src/javascripts/main.js",
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'javascripts/main.js'
+    path: path.resolve(__dirname, "./dist"),
+    filename: "javascripts/main.js",
   },
   module: {
     rules: [
       {
-        test: /\.css/,
+        test: /\.(css|sass|scss)/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
           },
+          {
+            loader: 'sass-loader',
+          }
         ],
       },
       {
         test: /\.(png|jpg)/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
               esModule: false,
-              name: 'images/[name].[ext]',
-            }
+              name: "images/[name].[ext]",
+            },
           },
         ],
       },
@@ -40,31 +43,34 @@ module.exports = {
         test: /\.pug/,
         use: [
           {
-            loader: 'html-loader',
+            loader: "html-loader",
           },
           {
-            loader: 'pug-html-loader',
+            loader: "pug-html-loader",
             options: {
               pretty: true,
-            }
-          }
-        ]
-      }
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: './stylesheets/my.css',
+      filename: "./stylesheets/main.css",
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.pug',
-      filename: 'index.html',
+      template: "./src/templates/index.pug",
+      filename: "index.html",
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/access.pug',
-      filename: 'access.html',
+      template: "./src/templates/access.pug",
+      filename: "access.html",
     }),
-
+    new HtmlWebpackPlugin({
+      template: "./src/templates/members/taro.pug",
+      filename: "members/taro.html",
+    }),
     new CleanWebpackPlugin(),
   ],
-}
+};
